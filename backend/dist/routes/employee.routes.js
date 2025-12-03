c842cb024ba1f012e8e0a4bd14b3731497a7892b
MariyaKingcilin <MariaKingcilin@tritonlabs.in>
2025-12-03 12:35:45 +0530

    Remove backend dist files and directories to clean up the project structure

--- backend/dist/routes/employee.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const employee_controller_1 = require("../controllers/employee.controller");
-const employeeRouter = (0, express_1.Router)();
-employeeRouter.get("/", employee_controller_1.getAllEmployees);
-employeeRouter.post("/create-employee", employee_controller_1.createEmployee);
-exports.default = employeeRouter;
